9325187a4e3dc97e3e507265923dc2fa327692a0
lukeingawesome <kolucas1219@gmail.com>
2025-07-15 12:52:57 +0900

    deleted unneeded files

--- run_all_exp.sh
+++
@@ -1,6 +0,0 @@
-# scripts/launch_sweep.sh
-#!/usr/bin/env bash
-source "$(dirname "$0")/env.sh"
-wandb sweep scripts/sweep.yaml | tee sweep_id.txt
-SWEEP_ID=$(tail -n1 sweep_id.txt | awk '{print $NF}')
-wandb agent $SWEEP_ID --count 30
